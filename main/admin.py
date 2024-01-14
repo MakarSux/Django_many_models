10e3874342fcbc1b9dc3751aa1d4abe308549af1
MakarSux <mm.pyshkin@yandex.ru>
2024-01-15 02:13:45 +0300

    create models

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Products, Orders, Customers
 
-# Register your models here.
+admin.site.register(Products)
+admin.site.register(Orders)
+admin.site.register(Customers)
